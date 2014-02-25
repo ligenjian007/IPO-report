01d6accec4f2337fc2badb08bf8df0b275dea1ef
ligenjian007 <ligenjian@rinejiandeMacBook-Pro.local>
2014-02-25 14:05:48 +0800

    add test push to mongo db

---
+++ testPdf2Mongo.py
@@ -0,0 +1,18 @@
+__author__ = 'ligenjian'
+
+import IPOReportExtractor
+import re
+import os
+import pymongo
+
+connection = pymongo.Connection('localhost', 27017)
+db = connection.risk_database
+collection = db.risk_collection
+
+pdfMatcher = re.compile(r'.*\.pdf')
+
+for filename in os.listdir('.'):
+    if pdfMatcher.match(filename):
+        ipoExtractor = IPOReportExtractor.IPOReportExtractor()
+        riskMap = ipoExtractor.processFile(filename, '.')
+        collection.insert({'corporation_name':filename.split('.')[0], 'risk_content' : riskMap})
